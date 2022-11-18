017ff1aa23fc8bc602db867952070a5100567e3c
Sanity.io <hello@sanity.io>
2022-11-18 22:56:41 +0100

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,52 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'args-sanity-studio',
+                  apiId: 'a1395e45-eb97-4f71-baac-ea0f4a99c97f'
+                },
+                {
+                  buildHookId: '',
+                  title: 'Blog Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/olagberg/args-sanity',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: '', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
